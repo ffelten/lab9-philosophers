2f2993b80551f372dfd787d2b22786794739a500
Florian Felten <>
2022-09-26 15:48:55 +0200

    Initial commit

---
+++ bin/philosophers.ml
@@ -0,0 +1,31 @@
+let number = 2 in 
+let philosopher id = 
+  let left = id in
+  let right = (id + 1) mod number in
+  let eat () = 
+    Printf.printf "Philosopher %d is eating plate using forks left:%d, right:%d.\n" id left right;
+    Thread.delay 0.3;
+    Printf.printf "Philosopher %d has finished eating.\n" id;
+    flush stdout;
+  in
+
+  let think () = 
+    Printf.printf "Philosopher %d is thinking.\n" id;
+    flush stdout;
+    Thread.delay 0.2; 
+  in
+
+  let rec loop alt = 
+    if alt then 
+      begin
+        eat ();
+        loop false
+      end
+    else 
+      think ();
+      loop true
+  in
+    loop true;
+in
+let threads = List.init number (fun n -> Thread.create philosopher n) in
+Thread.join (List.hd threads)
