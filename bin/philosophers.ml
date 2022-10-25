358b69465db07f329dc17aa6a81566e255ce488e
Florian Felten <>
2022-10-25 17:00:02 +0200

    Refactor template

--- bin/philosophers.ml
+++ bin/philosophers.ml
@@ -1,32 +1,33 @@
-let number = 2 in 
-let philosopher id = 
+let philosopher number id =
   let left = id in
   let right = (id + 1) mod number in
-  
-  let eat () = 
+
+  let eat () =
     Printf.printf "Philosopher %d is eating plate using forks left:%d, right:%d.\n" id left right;
     Thread.delay 0.3;
     Printf.printf "Philosopher %d has finished eating.\n" id;
-    flush stdout;
+    flush stdout
   in
 
-  let think () = 
+  let think () =
     Printf.printf "Philosopher %d is thinking.\n" id;
     flush stdout;
-    Thread.delay 0.2; 
+    Thread.delay 0.2
   in
 
-  let rec loop alt = 
-    if alt then 
+  let rec loop alt =
+    if alt then
       begin
         eat ();
         loop false
       end
-    else 
+    else
       think ();
       loop true
   in
-    loop true;
-in
-let threads = List.init number (fun n -> Thread.create philosopher n) in
-Thread.join (List.hd threads)
+    loop true
+
+let _ =
+  let number = 2 in
+  let threads = List.init number (fun n -> Thread.create (philosopher number) n) in
+  Thread.join (List.hd threads)
